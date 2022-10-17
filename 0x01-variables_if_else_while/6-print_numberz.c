4321def29a89b37d5e38d79f813dd66cb8e878d3
AdeOluwatobi <AdeOluwatobi>
2022-10-17 14:59:02 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - program entry point.
+* Return: 0 no error, non zero if error
+*/
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
