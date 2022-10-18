60bb668ff04bc5c5926ef8d8d3516792ed12ba4a
AdeOluwatobi <AdeOluwatobi>
2022-10-18 07:58:42 -0700

    main.h file created

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+/**
+ * file - main.h
+ * Author: AdeOluwatobi
+ * Desc: Header contains prototypes for all functions
+ * 	used in the project 0x02. C - Functions, nested loops directory
+ */
+
+int _putchar(char c);
+
+
+
+
+
+#endif
